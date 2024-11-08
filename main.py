060ab7948ecb5441dc1c71edbb9340c9b09c984d
Brian Cerda <cerdabrian26@gmail.com>
2024-11-08 15:33:51 -0600

    Created two buttons for the tkinter window.

--- main.py
+++ main.py
@@ -3,11 +3,27 @@ import tkinter as tk
 
 class PinWindowApp:
     # TODO
-    def __init__(self, tkinter_window):
-        pass
+    def __init__(self, tk_window):
+        self.tk_window = tk_window
+        self.tk_window.title("Window Pin")
+
+        # List to keep track of all pinned windows.
+        self.pinned_windows = []
+
+        self.pin_button = tk.Button(tk_window, text="Pin Window", command=self.toggle_pin_process)
+        # pack() automatically places the widget (pin_button) inside the tk_window, stacking it from top to bottom.
+        # pady adds padding to the top, and bottom of the widget.
+        self.pin_button.pack(pady=10)
+
+        self.unpin_button = tk.Button(tk_window, text="Unpin Window", command=self.unpin_window)
+        self.unpin_button.pack(pady=10)
 
     # TODO
     def pin_window(self):
+        pass
+
+    #TODO
+    def toggle_pin_process(self):
         pass
 
     # TODO
