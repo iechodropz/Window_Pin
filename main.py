ecf4dd6a4bcb69a7318807ca00225f7303b00fa3
Brian Cerda <cerdabrian26@gmail.com>
2024-11-08 14:55:48 -0600

    Created initial code skeleton.

---
+++ main.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+
+class PinWindowApp:
+    # TODO
+    def __init__(self, tkinter_window):
+        pass
+
+    # TODO
+    def pin_window(self):
+        pass
+
+    # TODO
+    def unpin_window(self):
+        pass
+
+
+if __name__ == "__main__":
+    # Initializes a blank window where you can add widgets (like buttons, labels, etc.).
+    root = tk.Tk()
+    app = PinWindowApp(root)
+    # Keeps the application running and continuously checks for events (button clicks, mouse movements, keyboard input, etc.) to respond to them.
+    root.mainloop()
